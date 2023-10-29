49e6307bbced47b93148542d54dc67b73ac9010b
StirlingSmith <stirlingjamessmith@gmail.com>
2023-10-30 09:32:22 +1300

    max articles working

--- src/queryBuilder/calculate_pages.jl
+++ src/queryBuilder/calculate_pages.jl
@@ -1,5 +1,5 @@
 function calculate_pages(result, max_articles)
-    pages = ceil(min(total_articles,num_articles)//100) # 100 is the number of articles on each page
-    return convert(Int, page)
+    pages = ceil(min(result["articles"]["totalResults"],max_articles)//100) # 100 is the number of articles on each page
+    return convert(Int, pages)
 
 end
