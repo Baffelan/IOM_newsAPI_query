2bf5ea2f02268f7f15014fe83a249ff1b3edccb7
StirlingSmith <stirlingjamessmith@gmail.com>
2023-10-30 09:30:14 +1300

    rudimentary queries executing.

--- src/IOM_newsAPI_query.jl
+++ src/IOM_newsAPI_query.jl
@@ -1,5 +1,13 @@
 module IOM_newsAPI_query
+    using HTTP
+    using JSON
+    using Dates
+    
+    include("queryBuilder/action.jl")
+    include("queryBuilder/authentication.jl")
+    include("queryBuilder/calculate_pages.jl")
+    include("queryBuilder/execute_query.jl")
+    include("queryBuilder/write_query.jl")
 
-# Write your package code here.
-"htenosauhtoen"
+    include("query_newsapi.jl")
 end
--- src/query_newsapi.jl
+++ src/query_newsapi.jl
@@ -1,13 +1,14 @@
-function query_newsapi(user, dates,  kwargs...)
+function query_newsapi(user, dates, num_articles)
 
     query_str = write_query(user, dates)
-    res_pg_1 = execute_query(query_str, 1) # Query 1 first, as it returns the total number of articles from a query
+    res_pg_1 = execute_query(query_str, authentication(),1) # Query 1 first, as it returns the total number of articles from a query
 
     num_pages = calculate_pages(res_pg_1, num_articles)
 
-    res_pgs = execute_query(query_str, 2:num_pages) # In future, this might be parralellised
+    res_pgs = execute_query.([query_str],[authentication()], 2:num_pages) # In future, this might be parralellised
     vcat(res_pg_1["articles"]["results"], [res["articles"]["results"] for res in res_pgs]...)
     # URL = "http://eventregistry.org/api/v1/article/getArticles"
 
 end
+
 
