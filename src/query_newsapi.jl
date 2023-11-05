6c9c912f3f15559adf37899266b0687ec0765434
StirlingSmith <stirlingjamessmith@gmail.com>
2023-11-06 10:54:46 +1300

    removed line

--- src/query_newsapi.jl
+++ src/query_newsapi.jl
@@ -19,8 +19,6 @@ function query_newsapi(user, dates, num_articles)
 
     res_pgs = execute_query.([query_str],[authentication()], 2:num_pages) # In future, this might be parralellised
     vcat(res_pg_1["articles"]["results"], [res["articles"]["results"] for res in res_pgs]...)
-    # URL = "http://eventregistry.org/api/v1/article/getArticles"
-
 end
 
 
