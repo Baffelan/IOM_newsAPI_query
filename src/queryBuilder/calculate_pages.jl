eb386db5c5115f61f1adbba21a389d2d044e97a6
StirlingSmith <stirlingjamessmith@gmail.com>
2023-11-27 15:05:09 +1300

    If statement chechking some articles were found

--- src/queryBuilder/calculate_pages.jl
+++ src/queryBuilder/calculate_pages.jl
@@ -1,5 +1,8 @@
 function calculate_pages(result, max_articles)
-    pages = ceil(min(result["articles"]["totalResults"],max_articles)//100) # 100 is the number of articles on each page
-    return convert(Int, pages)
-
+    if length(result)!=0
+        pages = ceil(min(result["articles"]["totalResults"],max_articles)//100) # 100 is the number of articles on each page
+        return convert(Int, pages)
+    else
+        return 0
+    end
 end
